1cac344a864d75f679874af0efc19896005961a0
Nguyen, Thanh Binh <thanh-binh.nguyen@strato.de>
2026-03-03 14:48:20 +0100

    Edited HelloWorld to not use Vuetify logos

--- app/components/HelloWorld.vue
+++ app/components/HelloWorld.vue
@@ -1,57 +1,22 @@
 <template>
   <v-container class="fill-height d-flex align-center flex-wrap" max-width="900">
-    <div>
-      <v-img
-        class="mb-4"
-        height="150"
-        src="~/assets/logo.png"
-      />
+    <div class="mx-auto">
+      <v-img class="mb-4" height="200" src="/ca_program.png" />
 
       <div class="mb-8 text-center">
         <div class="text-body-medium font-weight-light mb-n1">Welcome to</div>
-        <h1 class="text-display-medium font-weight-bold">Vuetify</h1>
+        <h1 class="text-display-medium font-weight-bold">Wurzelheim</h1>
       </div>
 
       <v-row>
         <v-col cols="12">
           <v-card
             class="py-4"
+            max-width="600"
             color="surface-variant"
-            image="https://cdn.vuetifyjs.com/docs/images/one/create/feature.png"
-            prepend-icon="mdi-rocket-launch-outline"
+            prepend-icon="mdi-pokeball"
             rounded="lg"
-            variant="tonal"
-          >
-            <template #image>
-              <v-img position="top right" />
-            </template>
-
-            <template #title>
-              <h2 class="text-headline-small font-weight-bold">
-                Get started
-              </h2>
-            </template>
-
-            <template #subtitle>
-              <div class="text-body-large">
-                Change this page by updating <v-kbd>{{ `<HelloWorld />` }}</v-kbd> in <v-kbd>components/HelloWorld.vue</v-kbd>.
-              </div>
-            </template>
-          </v-card>
-        </v-col>
-
-        <v-col v-for="link in links" :key="link.href" cols="6">
-          <v-card
-            append-icon="mdi-open-in-new"
-            class="py-4"
-            color="surface-variant"
-            :href="link.href"
-            :prepend-icon="link.icon"
-            rel="noopener noreferrer"
-            rounded="lg"
-            :subtitle="link.subtitle"
-            target="_blank"
-            :title="link.title"
+            title="Tools to make life for Community Ambassadors easier"
             variant="tonal"
           />
         </v-col>
@@ -60,31 +25,4 @@
   </v-container>
 </template>
 
-<script setup>
-  const links = [
-    {
-      href: 'https://vuetifyjs.com/',
-      icon: 'mdi-text-box-outline',
-      subtitle: 'Learn about all things Vuetify in our documentation.',
-      title: 'Documentation',
-    },
-    {
-      href: 'https://vuetifyjs.com/introduction/why-vuetify/#feature-guides',
-      icon: 'mdi-star-circle-outline',
-      subtitle: 'Explore available framework Features.',
-      title: 'Features',
-    },
-    {
-      href: 'https://vuetifyjs.com/components/all',
-      icon: 'mdi-widgets-outline',
-      subtitle: 'Discover components in the API Explorer.',
-      title: 'Components',
-    },
-    {
-      href: 'https://discord.vuetifyjs.com',
-      icon: 'mdi-account-group-outline',
-      subtitle: 'Connect with Vuetify developers.',
-      title: 'Community',
-    },
-  ]
-</script>
+<script setup></script>
