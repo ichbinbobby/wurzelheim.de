1e98a96e1226829f9c1bc8723cfcbf3143453c34
Thanh Binh Nguyen <thanh-binh.nguyen@strato.de>
2026-05-19 14:53:41 +0200

    feat: add Google Tag Manager (GTM-WQKZ7SDN)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,17 +12,17 @@ export default defineNuxtConfig({
   // when enabling ssr option you need to disable inlineStyles and maybe devLogs
   features: {
     inlineStyles: false,
-    devLogs: false,
+    devLogs: false
   },
 
   build: {
-    transpile: ['vuetify'],
+    transpile: ['vuetify']
   },
 
   vite: {
     ssr: {
-      noExternal: ['vuetify'],
-    },
+      noExternal: ['vuetify']
+    }
   },
 
   css: [],
@@ -37,15 +37,37 @@ export default defineNuxtConfig({
         prefersColorScheme: false,
 
         prefersColorSchemeOptions: {
-          useBrowserThemeOnly: false,
-        },
+          useBrowserThemeOnly: false
+        }
       },
 
       // /* If customizing sass global variables ($utilities, $reset, $color-pack, $body-font-family, etc) */
       // disableVuetifyStyles: true,
       styles: {
-        configFile: 'assets/settings.scss',
-      },
-    },
+        configFile: 'assets/settings.scss'
+      }
+    }
   },
+
+  app: {
+    head: {
+      script: [
+        {
+          innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WQKZ7SDN');`,
+          tagPosition: 'head'
+        }
+      ],
+      noscript: [
+        {
+          innerHTML: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WQKZ7SDN"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          tagPosition: 'bodyOpen'
+        }
+      ]
+    }
+  }
 })
